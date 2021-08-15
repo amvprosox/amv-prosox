2e73587a0215bcc5ebed999028143567e52f75b0
amvprosox <84389974+amvprosox@users.noreply.github.com>
2021-08-15 19:55:55 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// MENU PANEL ACTIVE
+let btn = document.querySelector("#btn");
+        let sidebar = document.querySelector(".sidebar");
+
+        btn.onclick = function(){
+        sidebar.classList.toggle("active");
+        }
+// MENU PANEL ACTIVE END
+
+// Animation Start
+AOS.init({
+        offset: 400,
+        duration: 1000
+});
+
+// Animation End
+
+//TOGGLE START
+function toggle(){
+        var header = document.getElementById("Header")
+        header.classList.toggle('active')
+    }
+//TOGGLE END
